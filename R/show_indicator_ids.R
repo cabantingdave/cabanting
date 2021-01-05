8b6e33a76ce19665ffc233a4bc29f8d4e38dedc3
Francis Dave Cabanting <33571896+cabantingdave@users.noreply.github.com>
2021-01-05 21:59:15 +0800

    Add files via upload

--- R/show_indicator_ids.R
+++ R/show_indicator_ids.R
@@ -1,3 +1,15 @@
+#' Show UIS Indicators
+#'
+#' @description
+#' `show_indicator_ids` shows the indicators and their corresponding indicator IDs on the current dataframe
+#'
+#' @param df The `data.frame` to be filtered. Leave the UIS dataset as is
+#' @name show_indicator_ids
+#' @rdname show_indicator_ids
+#' @md
+#' @export
+
+
 show_indicator_ids <- function(df){
   df <- df[match(unique(df[,1]),df[,1]),c(1,2)]
   colnames(df) <- c("INDICATOR_ID","INDICATOR")
