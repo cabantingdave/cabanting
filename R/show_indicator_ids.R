6729ebb4b7ff74ed02bfc7386642fa640d48a23e
Francis Dave Cabanting <33571896+cabantingdave@users.noreply.github.com>
2021-01-05 22:12:16 +0800

    Update show_indicator_ids.R

--- R/show_indicator_ids.R
+++ R/show_indicator_ids.R
@@ -13,6 +13,7 @@
 show_indicator_ids <- function(df){
   df <- df[match(unique(df[,1]),df[,1]),c(1,2)]
   colnames(df) <- c("INDICATOR_ID","INDICATOR")
-  indicators <- setNames(df[,2],df[,1])
+  indicators <- array(data.frame(setNames(df[,2],df[,1])))
   indicators
+  
 }
